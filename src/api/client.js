feaa85c95889905ca53448e77ea7e984859a09a3
olga <goncherenkoola@gmail.com>
2023-08-13 20:07:31 +0300

    change url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+//https://goldfish-app-zgu69.ondigitalocean.app/
 const client = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://goldfish-app-zgu69.ondigitalocean.app/api",
 });
 
 export default client;
